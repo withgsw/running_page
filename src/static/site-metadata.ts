c7d05de13f465399c1a11754b676dec260ed99d8
Max <withgsw@outlook.com>
2025-08-11 19:23:07 +0800

    update

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -16,7 +16,7 @@ const getBasePath = () => {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Max Running',
-  siteUrl: 'https://yihong.run',
+  siteUrl: 'https://withgsw.github.io/running_page/',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
   navLinks: [
